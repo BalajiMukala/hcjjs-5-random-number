ef16d65a891e2ea7c7a97c28ff14944105e48ac5
bmm5533 <134575255+bmm5533@users.noreply.github.com>
2024-07-05 16:07:27 +0530

    final chek

--- src/script.js
+++ src/script.js
@@ -1,1 +1,19 @@
-console.log(`${Date()} :: This is a starter template for a simple web app.`);
+document.addEventListener("DOMContentLoaded", () => {
+    const number = document.querySelector(".number");
+    const generateBtn = document.querySelector(".generate");
+    const resetBtn = document.querySelector(".reset");
+
+    const generateNumber = () => {
+        const randomNumber = Math.floor(Math.random() * 1001);
+        number.textContent = randomNumber;
+    };
+
+    const resetNumber = () => {
+        number.textContent = 0;
+    };
+
+    generateBtn.addEventListener("click", generateNumber);
+    resetBtn.addEventListener("click", resetNumber);
+
+    generateNumber();
+});
